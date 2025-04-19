07afb9fdfddf9fc21cc119d4308b9bed816d159b
DavidA346 <davidaispuro346@gmail.com>
2025-04-18 23:02:36 -0700

    Added styling for the bottom tab bar

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,21 +1,97 @@
 import { Tabs } from "expo-router";
+import { Image, Text } from "react-native";
 
 const TabsLayout = () => {
     return (
-        <Tabs>
+        <Tabs
+        screenOptions={{
+            tabBarStyle: {
+              backgroundColor: 'transparent',
+              elevation: 0,
+              borderTopWidth: 0,
+              position: 'absolute',
+            },
+          }}
+        >      
             <Tabs.Screen name="index"
             options={{
-                headerTitle: "Dragon Catcher",
+                headerTitle: "DRAGON CATCHER",
+                tabBarLabel: ({ focused }) => (
+                    <Text style={{
+                      fontSize: 20,
+                      color: focused ? 'black' : '#999',
+                      marginTop: 20,
+                    }}>
+                      HATCH
+                    </Text>
+                  ),
+
+                tabBarIcon: ({ focused }) => (
+                    <Image
+                        source={require("../../assets/Dragons/Red/egg_sprites/sprite_0.png")}
+                        style={{
+                            width: 70,
+                            height: 70,
+                            resizeMode: 'contain',
+                            opacity: focused ? 1 : 0.5,
+                            marginBottom: 20,
+                        }}
+                    />
+                )
             }}/>
 
             <Tabs.Screen name="shop"
             options={{
-                headerTitle: "Shop",
+                headerTitle: "STORE",
+                tabBarLabel: ({ focused }) => (
+                    <Text style={{
+                      fontSize: 20,
+                      color: focused ? 'black' : '#999',
+                      marginTop: 20,
+                    }}>
+                      STORE
+                    </Text>
+                  ),
+
+                tabBarIcon: ({ focused }) => (
+                    <Image
+                        source={require("../../assets/item sprites/coin/coin_sprite.png")}
+                        style={{
+                            width: 70,
+                            height: 70,
+                            resizeMode: 'contain',
+                            opacity: focused ? 1 : 0.5,
+                            marginBottom: 20,
+                        }}
+                    />
+                )
             }}/>
 
              <Tabs.Screen name="inventory"
             options={{
-                headerTitle: "Inventory",
+                headerTitle: "BROOD",
+                tabBarLabel: ({ focused }) => (
+                    <Text style={{
+                      fontSize: 20,
+                      color: focused ? 'black' : '#999',
+                      marginTop: 20,
+                    }}>
+                      BROOD
+                    </Text>
+                  ),
+
+                tabBarIcon: ({ focused }) => (
+                    <Image
+                        source={require("../../assets/Dragons/Red/adult.gif")}
+                        style={{
+                            width: 70,
+                            height: 70,
+                            resizeMode: 'contain',
+                            opacity: focused ? 1 : 0.5,
+                            marginBottom: 20,
+                        }}
+                    />
+                )
             }}/>
         </Tabs>
     );
