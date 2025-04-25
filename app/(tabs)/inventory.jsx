f822f7605d51f108e37ab7435f94a6f4f39fdf86
jahooba <josh.uapena20002@gmail.com>
2025-04-25 12:27:44 -0700

    Updated screen *collection is now only for the 10 creatures *flatList for each species

--- app/(tabs)/inventory.jsx
+++ app/(tabs)/inventory.jsx
@@ -1,51 +1,23 @@
 import { StyleSheet, Text, View, ImageBackground, Image, FlatList, Pressable } from "react-native"
 
 //Holds the dragons in their cards
-const firstDragonRow = [
-    { id: '1', image: require('../../assets/Wyverns/Blue-Green/adult.gif')},
-    { id: '2', image: require('../../assets/Dragons/White-Violet/baby.gif')},
-    { id: '3', image: require('../../assets/Dragons/Gold/adult.gif')},
+const dragonCollection = [
+    { id: '1', image: require('../../assets/Dragons/Blue-Orange/adult.gif')},
+    { id: '2', image: require('../../assets/Dragons/Gold/adult.gif')},
+    { id: '3', image: require('../../assets/Dragons/Green/baby.gif')},
+    { id: '4', image: require('../../assets/Dragons/Orange/baby.gif')},
+    { id: '5', image: require('../../assets/Dragons/Red/adult.gif')},
+    { id: '6', image: require('../../assets/Dragons/White-Violet/baby.gif')}
 ];
 
-const secondDragonRow = [
-    { id: '1', image: require('../../assets/Wyverns/Brown-Green/adult.gif')},
-    { id: '2', image: require('../../assets/Dragons/Orange/adult.gif')},
-    { id: '3', image: require('../../assets/Dragons/Blue-Orange/adult.gif')},
+const drakeCollection = [
+    { id: '7', image: require('../../assets/Drakes/Black-Violet/baby.gif')},
+    { id: '8', image: require('../../assets/Drakes/Green-Red/adult.gif')}
 ];
 
-const thirdDragonRow = [
-    { id: '1', image: require('../../assets/Drakes/Black-Violet/adult.gif')},
-    { id: '2', image: require('../../assets/Wyverns/Blue-Green/baby.gif')},
-    { id: '3', image: require('../../assets/Dragons/Green/adult.gif')},
-];
-
-const fourthDragonRow = [
-    { id: '1', image: require('../../assets/Drakes/Green-Red/adult.gif')},
-    { id: '2', image: require('../../assets/Dragons/White-Violet/adult.gif')},
-    { id: '3', image: require('../../assets/Dragons/Red/adult.gif')},
-];
-
-const dragonCollection = [{ id: '1', image: require('../../assets/Wyverns/Blue-Green/adult.gif')},
-                          { id: '2', image: require('../../assets/Dragons/White-Violet/baby.gif')},
-                          { id: '3', image: require('../../assets/Dragons/Gold/adult.gif')},
-                          { id: '4', image: require('../../assets/Wyverns/Brown-Green/adult.gif')},
-                          { id: '5', image: require('../../assets/Dragons/Orange/adult.gif')},
-                          { id: '6', image: require('../../assets/Dragons/Blue-Orange/adult.gif')},
-                          { id: '7', image: require('../../assets/Drakes/Black-Violet/adult.gif')},
-                          { id: '8', image: require('../../assets/Wyverns/Blue-Green/baby.gif')},
-                          { id: '9', image: require('../../assets/Dragons/Green/adult.gif')},
-                          { id: '10', image: require('../../assets/Drakes/Green-Red/adult.gif')},
-                          { id: '11', image: require('../../assets/Dragons/White-Violet/adult.gif')},
-                          { id: '12', image: require('../../assets/Dragons/Red/adult.gif')},
-                          { id: '13', image: require('../../assets/Wyverns/Blue-Green/adult.gif')},
-                          { id: '14', image: require('../../assets/Dragons/White-Violet/baby.gif')},
-                          { id: '15', image: require('../../assets/Dragons/Gold/adult.gif')},
-                          { id: '16', image: require('../../assets/Wyverns/Brown-Green/adult.gif')},
-                          { id: '17', image: require('../../assets/Dragons/Orange/adult.gif')},
-                          { id: '18', image: require('../../assets/Dragons/Blue-Orange/adult.gif')},
-                          { id: '19', image: require('../../assets/Drakes/Black-Violet/adult.gif')},
-                          { id: '20', image: require('../../assets/Wyverns/Blue-Green/baby.gif')},
-                          { id: '21', image: require('../../assets/Dragons/Green/adult.gif')},
+const wyvernCollection = [
+    { id: '9', image: require('../../assets/Wyverns/Blue-Green/baby.gif')},
+    { id: '10', image: require('../../assets/Wyverns/Brown-Green/adult.gif')}
 ];
 
 const Inventory = () => {
@@ -85,9 +57,9 @@ const Inventory = () => {
                 <View style={styles.statSection}>
                     <View>
                         <Text style={styles.stats}>Totals</Text>
-                        <Text style={styles.dragonText}>Dragons: 9</Text>
-                        <Text style={styles.drakesText}>Drakes: 9</Text>
-                        <Text style={styles.wyvernsText}>Wyverns: 9</Text>
+                        <Text style={styles.dragonText}>Dragons: 6</Text>
+                        <Text style={styles.drakesText}>Drakes: 2</Text>
+                        <Text style={styles.wyvernsText}>Wyverns: 2</Text>
                     </View>
 
                     <View>
@@ -103,9 +75,27 @@ const Inventory = () => {
                             renderItem={renderItem}
                             keyExtractor={(item) => item.id}
                             numColumns={3}
-                            contentContainerStyle={styles.collection}
+                            contentContainerStyle={styles.dragonsGrid}
                             showsVerticalScrollIndicator={false}
-                />               
+                />
+
+                <FlatList  data={drakeCollection}
+                            renderItem={renderItem}
+                            keyExtractor={(item) => item.id}
+                            numColumns={2}
+                            contentContainerStyle={styles.drakesGrid}
+                            style={styles.TwoColumnList}
+                            showsVerticalScrollIndicator={false}
+                /> 
+
+                <FlatList  data={wyvernCollection}
+                            renderItem={renderItem}
+                            keyExtractor={(item) => item.id}
+                            numColumns={2}
+                            contentContainerStyle={styles.wyvernsGrid}
+                            style={styles.TwoColumnList}
+                            showsVerticalScrollIndicator={false}
+                />              
             </View>
         </ImageBackground>
     )
@@ -141,34 +131,47 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
       },
 
-      collection: {
-        // backgroundColor: '#fff',
-        paddingBottom: 40,
-        paddingHorizontal: 20,
-        paddingLeft: 1,
-        justifyContent: 'center',
-      },
+    dragonsGrid: {
+    // backgroundColor: '#fff',
+    paddingHorizontal: 20,
+    paddingLeft: 1,
+    justifyContent: 'center',
+    },
 
-      image: {
-        width: 100,
-        height: 100,
-        marginLeft: '25%',
-        // alignItem: 'center'
-        // borderRadius: 8,
-        // resizeMode: 'contain'
-      },
+    TwoColumnList: {
+    // backgroundColor: '#fff',
+    width: '125%'
+    },
 
-      stats: {
-        fontWeight: 'bold',
-        textDecorationLine: 'underline',
-        fontSize: 18,
-      },
+    drakesGrid: {
+    // backgroundColor: '#fff',
+    paddingLeft: '23%',
+    justifyContent: 'center',
+    },
 
-      statSection: {
-        flexDirection: 'row',
-        gap: 120,
-        paddingBottom: 10,
-      },
+    wyvernsGrid: {
+    // backgroundColor: '#fff',
+    paddingLeft: '23%',
+    justifyContent: 'center',
+    },
+
+    image: {
+    width: 100,
+    height: 100,
+    marginLeft: '25%'
+    },
+
+    stats: {
+    fontWeight: 'bold',
+    textDecorationLine: 'underline',
+    fontSize: 18,
+    },
+
+    statSection: {
+    flexDirection: 'row',
+    gap: 120,
+    paddingBottom: 10,
+    },
 
     dragonText: {
         fontSize: 18,
